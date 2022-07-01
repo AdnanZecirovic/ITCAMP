ddafd436451df6ccfb5832345cf8f95d8ed30627
Adnan Zecirovic <adnanzecirovic@gmail.com>
2022-07-01 12:22:07 +0200

    second exercise was great

---
+++ Vezba14.1HomeExercises/exercises.js
@@ -0,0 +1,17 @@
+// 1. Write a JavaScript program to display the current day and time
+const dan = new Date();
+dan.setDate(dan.getDate());
+console.log("Today is: " + dan);
+
+//Write a JavaScript program to rotate the string 'w3resource' in right direction by periodically removing one letter from the end of the string and attaching it to the front
+let nameOfString = "w3resource";
+console.log(nameOfString.length);
+// for (let i = nameOfString.length; i > 0; i--) {
+//   nameOfString.slice(i);
+//   console.log(nameOfString);
+// }
+let niz = "";
+for (let i = nameOfString.length; i >= 0; i--) {
+  niz += nameOfString.charAt(i);
+}
+console.log(niz);
