33d2dd22223c4413cc1fc9265a388d4e51ec0b1b
Adnan Zecirovic <adnanzecirovic@gmail.com>
2022-07-03 13:59:29 +0200

    JS button for screen printing

--- Vezba14.1HomeExercises/exercises.js
+++ Vezba14.1HomeExercises/exercises.js
@@ -3,9 +3,10 @@ const dan = new Date();
 dan.setDate(dan.getDate());
 console.log("Today is: " + dan);
 
-//Write a JavaScript program to rotate the string 'w3resource' in right direction by periodically removing one letter from the end of the string and attaching it to the front
+//Write a JavaScript program to rotate the string 'w3resource' in right direction by
+//periodically removing one letter from the end of the string and attaching it to the front
 let nameOfString = "w3resource";
-console.log(nameOfString.length);
+// console.log(nameOfString.length);
 // for (let i = nameOfString.length; i > 0; i--) {
 //   nameOfString.slice(i);
 //   console.log(nameOfString);
