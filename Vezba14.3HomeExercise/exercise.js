cd2e070217a5d7b1f17bfce9340c625fd0f47993
Adnan Zecirovic <adnanzecirovic@gmail.com>
2022-07-03 14:48:35 +0200

    simple app that takes values from 2 fields and does basic operations

---
+++ Vezba14.3HomeExercise/exercise.js
@@ -0,0 +1,21 @@
+function addition() {
+  let a = parseInt(document.getElementById("firstNumber").value);
+  let b = parseInt(document.getElementById("secondNumber").value);
+  document.getElementById("result").value = a + b;
+}
+function subtraction() {
+  let a = parseInt(document.getElementById("firstNumber").value);
+  let b = parseInt(document.getElementById("secondNumber").value);
+  document.getElementById("result").value = a - b;
+}
+function multiplication() {
+  let a = parseInt(document.getElementById("firstNumber").value);
+  let b = parseInt(document.getElementById("secondNumber").value);
+  document.getElementById("result").value = a * b;
+}
+function division() {
+  let a = parseInt(document.getElementById("firstNumber").value);
+  let b = parseInt(document.getElementById("secondNumber").value);
+  document.getElementById("result").value = a / b;
+}
+document.getElementById("result").value = result;
