b7f18f54dce46758710e083ce2d28bb689762d77
Adnan Zecirovic <adnanzecirovic@gmail.com>
2022-08-02 20:56:42 +0200

    started javascript finally

---
+++ Vezba17/exercise.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+//data types: primitivni i referentni
+//primitivni su brojevi, stringovi, boolean
+//referentni su funkcije itd
+let vrednost = 10;
+console.log(vrednost);
+console.log(vrednost);
+console.log(vrednost);
+console.log(vrednost);
