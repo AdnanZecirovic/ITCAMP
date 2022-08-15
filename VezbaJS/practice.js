d295e87306ef70982e2340f97e3ab4001f80fe21
zecirovicado <adnanzecirovic@gmail.com>
2022-08-16 01:15:15 +0200

    fine details

--- VezbaJS/practice.js
+++ VezbaJS/practice.js
@@ -2,15 +2,16 @@
 let unos = prompt("Enter some text");
 let array1 = [];
 let array2 = [];
-for (let i = 0; i < unos.length; i++) {
-  array1.push(unos[i]);
+let unos1 = unos.replace(/\s+/g, "");
+for (let i = 0; i < unos1.length; i++) {
+  array1.push(unos1[i]);
 }
 //console.log(array1);
-for (let i = unos.length - 1; i >= 0; i--) {
-  array2.push(unos[i]);
+for (let i = unos1.length - 1; i >= 0; i--) {
+  array2.push(unos1[i]);
 }
 //console.log(array2);
-for (let i = 0; i < unos.length; i++) {
+for (let i = 0; i < unos1.length; i++) {
   if (array1[i] === array2[i]) {
     console.log("This is a palindrome");
   } else {
