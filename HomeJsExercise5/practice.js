7eab1fddf392254e4bd933e66e0e91191c383a69
zecirovicado <adnanzecirovic@gmail.com>
2022-08-16 11:25:48 +0200

    for loop exercise 5

---
+++ HomeJsExercise5/practice.js
@@ -0,0 +1,7 @@
+//Write a program in C to display the cube of the number upto given an integer.
+let cube = Number(prompt("Input a number"));
+for (let i = 1; i <= cube; i++) {
+  console.log(
+    "Number is: " + i + " and the cube of the " + i + " is: " + i * i * i
+  );
+}
