8b8ba9a8808885df349a8de941bbbf11c1e8b7be
zecirovicado <adnanzecirovic@gmail.com>
2022-08-16 11:29:48 +0200

    for loop exercise 6

---
+++ HomeJsExercise6/practice.js
@@ -0,0 +1,5 @@
+//Write a program in C to display the multiplication table of a given integer.
+let input = Number(prompt("Input a number"));
+for (let i = 1; i <= 10; i++) {
+  console.log(input + " X " + i + " = " + input * i);
+}
