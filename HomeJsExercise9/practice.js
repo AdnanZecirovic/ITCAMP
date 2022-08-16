d7509c64e59351388b45a8a4c85d1a1e335ad90e
zecirovicado <adnanzecirovic@gmail.com>
2022-08-16 12:35:55 +0200

    for loop exercise 9

---
+++ HomeJsExercise9/practice.js
@@ -0,0 +1,6 @@
+//Write a program in C to display the pattern like right angle triangle using an asterisk.
+let array = [];
+for (let i = 1; i <= 4; i++) {
+  array.push("*");
+  console.log(array);
+}
