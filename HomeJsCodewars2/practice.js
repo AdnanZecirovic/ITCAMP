3724eb1796f167f445943df398f98f698efd09dc
zecirovicado <adnanzecirovic@gmail.com>
2022-08-24 23:07:54 +0200

    CodeWars3 - Complete

--- HomeJsCodewars2/practice.js
+++ HomeJsCodewars2/practice.js
@@ -2,5 +2,6 @@ function removeChar(str) {
   let word = String(str);
   let newWord = word.slice(0, word.length - 1);
   newWord = newWord.slice(1);
+  console.log(newWord);
   return newWord;
 }
---
+++ HomeJsCodewars3/practice.js
@@ -0,0 +1,5 @@
+function makeUpperCase(str) {
+  let upper = String(str);
+  console.log(upper.toUpperCase());
+  return upper.toUpperCase();
+}
